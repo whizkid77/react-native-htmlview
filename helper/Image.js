65f3cb9d497546e85beb978e2b3d7a0729ad8bb2
James Lin <james@shopworks.io>
2016-07-19 22:35:29 -0700

    update for latest RN version

--- helper/Image.js
+++ helper/Image.js
@@ -1,8 +1,8 @@
-var React = require('react-native')
+var ReactNative = require('react-native')
 var {
   Image,
   Dimensions,
-} = React
+} = ReactNative
 
 var {width} = Dimensions.get('window')
 
